378725375bddbd1b21a059e0eedf64a9b73004da
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-02-14 19:21:18 -0600

    Starter program file

--- Program.fs
+++ Program.fs
@@ -1,13 +1,28 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-open System
+let parse source =
+    let buf =
+        FSharp.Text.Lexing.LexBuffer<_>.FromString source
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+    try
+        let expr = Parser.start (Lexer.tokenize) buf
+        expr |> ignore
+    with
+    | :? Parser.ParseError as e ->
+        let (tok, t1, t2) = e.Data
+        printfn "Error: %0" tok
+        printfn "Expected: %0" [ for t in t1 @ t2 -> Parser.tokenTagToTokenId t ]
+    | e -> printfn "Error: %0" e.Message
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let rec loop s =
+        printf "kaleidoscope> "
+
+        match System.Console.ReadLine() with
+        | null -> 0
+        | s ->
+            parse s
+            loop s
+
+    loop ""
