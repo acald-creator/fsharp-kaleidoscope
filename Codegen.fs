dafef4c459c4d357e82b9a1cf76a38e34bc10f92
Antonette Caldwell <18711313+acald-creator@users.noreply.github.com>
2026-05-21 01:04:59 -0500

    Upgrade LLVMSharp to 20.1.2 and fix runtime native initialization

--- Codegen.fs
+++ Codegen.fs
@@ -1,11 +1,21 @@
+#nowarn "9"
+#nowarn "3391"
 module Codegen
 
-open LLVMSharp
+open LLVMSharp.Interop
 open AST
 open System.Collections.Generic
+open Microsoft.FSharp.NativeInterop
 
 let context = LLVM.GetGlobalContext()
-let themodule = LLVM.ModuleCreateWithNameInContext("module", context)
+
+let withString (s: string) (f: nativeptr<sbyte> -> 'a) : 'a =
+    let bytes = System.Text.Encoding.UTF8.GetBytes(s + "\u0000")
+    use p = fixed bytes
+    let sptr = p |> NativePtr.toVoidPtr |> NativePtr.ofVoidPtr
+    f sptr
+
+let themodule = withString "module" (fun name -> LLVM.ModuleCreateWithNameInContext(name, context))
 let builder = LLVM.CreateBuilderInContext(context)
 
 let namevalues = Dictionary<string, LLVMValueRef>()
@@ -19,35 +29,42 @@ let rec codegen expr =
     | Expr.Variable n -> namevalues.[n]
     | Expr.Binop((op, _), lhs, rhs) ->
         match op with
-        | "+" -> LLVM.BuildFAdd(builder, !lhs, !rhs, "addtmp")
-        | "-" -> LLVM.BuildFSub(builder, !lhs, !rhs, "subtmp")
-        | "*" -> LLVM.BuildFMul(builder, !lhs, !rhs, "multmp") 
+        | "+" -> withString "addtmp" (fun name -> LLVM.BuildFAdd(builder, !lhs, !rhs, name))
+        | "-" -> withString "subtmp" (fun name -> LLVM.BuildFSub(builder, !lhs, !rhs, name))
+        | "*" -> withString "multmp" (fun name -> LLVM.BuildFMul(builder, !lhs, !rhs, name))
         | "<" ->
-            let boolVal = LLVM.BuildFCmp(builder, LLVMRealPredicate.LLVMRealULT, !lhs, !rhs, "cmptmp") 
-            LLVM.BuildUIToFP(builder, boolVal, LLVM.DoubleTypeInContext(context), "booltmp")
+            let boolVal = withString "cmptmp" (fun name -> LLVM.BuildFCmp(builder, LLVMRealPredicate.LLVMRealULT, !lhs, !rhs, name))
+            withString "booltmp" (fun name -> LLVM.BuildUIToFP(builder, boolVal, LLVM.DoubleTypeInContext(context), name))
         | _ -> failwithf "Invalid Operator %s" op
-    | Expr.Call(callee, args) -> LLVM.BuildCall(builder, !callee, !! args, "calltmp")
+    | Expr.Call(callee, args) ->
+        let calleeVal = !callee
+        let argsArray = !!args
+        use argsPtr = fixed argsArray
+        let fnTy = LLVM.GlobalGetValueType(calleeVal)
+        withString "calltmp" (fun name -> LLVM.BuildCall2(builder, fnTy, calleeVal, argsPtr, uint32 argsArray.Length, name))
     | Expr.Func(name, param, body) ->
         let func =
             let typ = [| for _ in param -> LLVM.DoubleTypeInContext(context) |]
-            let ty = LLVM.FunctionType(LLVM.DoubleTypeInContext(context), typ, false) 
-            LLVM.AddFunction(themodule, name, ty)
+            use typPtr = fixed typ
+            let ty = LLVM.FunctionType(LLVM.DoubleTypeInContext(context), typPtr, uint32 typ.Length, 0)
+            withString name (fun nptr -> LLVM.AddFunction(themodule, nptr, ty))
         namevalues.[name] <- func
         for i, p in List.indexed param do 
-            namevalues.[p] <- func.GetParam(uint32 i)
-        let bb = LLVM.AppendBasicBlock(func, "entry")
+            namevalues.[p] <- LLVM.GetParam(func, uint32 i)
+        let bb = withString "entry" (fun eptr -> LLVM.AppendBasicBlock(func, eptr))
         LLVM.PositionBuilderAtEnd(builder, bb) 
         let _ = LLVM.BuildRet(builder, !body)
         func
     | Expr.Extern(name, param) ->
         let func =
             let typ = [| for _ in param -> LLVM.DoubleTypeInContext(context) |]
-            let ty = LLVM.FunctionType(LLVM.DoubleTypeInContext(context), typ, false) 
-            LLVM.AddFunction(themodule, name, ty)
+            use typPtr = fixed typ
+            let ty = LLVM.FunctionType(LLVM.DoubleTypeInContext(context), typPtr, uint32 typ.Length, 0)
+            withString name (fun nptr -> LLVM.AddFunction(themodule, nptr, ty))
         namevalues.[name] <- func
         func
 
 let dump_ir expr =
     let value = codegen expr
-    value.Dump()
+    LLVM.DumpValue(value)
     printfn ""
